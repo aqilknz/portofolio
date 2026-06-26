802bf0e59491c92da2b53445d48b51a3245f1caf
aqilknz <khairunnz123@gmail.com>
2026-06-27 03:34:28 +0700

    feat: add certificates data

---
+++ src/data/certificatesData.js
@@ -0,0 +1,23 @@
+export const CERTIFICATES = [
+    {
+        id: 1,
+        name: "Linux System Administration",
+        issuer: "BTech Academy",
+        year: "2025",
+        link: "/certificates/linux-sertifikat.png",
+    },
+    {
+        id: 2,
+        name: "Docker & Container Orchestration",
+        issuer: "BTech Academy",
+        year: "2025",
+        link: "/certificates/linux-sertifikat.png",
+    },
+    {
+        id: 3,
+        name: "Fullstack Web Development",
+        issuer: "Koda Tech Academy",
+        year: "2026",
+        link: "/certificates/linux-sertifikat.png",
+    },
+];
